675b0fe760ebf67b63c7248b067a9c285d66643c
bhagyesh <bhagyesh@192.168.1.29>
2022-04-27 11:30:10 +0530

    Commit for Testops Integration

--- Test Suites/End To End Test Suite.ts
+++ Test Suites/End To End Test Suite.ts
@@ -5,10 +5,16 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>6d87338d-8242-4bd8-81af-f3d5ba097f23</testSuiteGuid>
+   <testCaseLink>
+      <guid>6fc34ce5-10fc-468e-99be-9e70588d8b27</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/End To End Test Flow</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
